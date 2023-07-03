74c2879a3e5d4d22fcf9ec4c48d84a125728b359
Devashish Suthar <44345744+DevashishSuthar@users.noreply.github.com>
2023-07-03 21:38:07 +0530

    Add show map button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useContext } from 'react';
+import { HiOutlineMap } from 'react-icons/hi'
 
 import ListingCard from "@/app/components/listings/ListingCard";
 import Container from '@/app/components/common/Container';
@@ -18,23 +19,43 @@ const Home = () => {
     return <EmptyState />;
   }
 
+  const handleShowMapClick = () => {
+    console.log('INSIDE HANDLE SHOW MAP CLICK')
+  }
+
   return (
-    <Container>
-      {location ?
-        <SearchResults /> :
-        (<div
-          className="pt-52 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8"
-        >
-          {data.map((listing: any, index: number) => (
-            <ListingCard
-              key={index}
-              listIndex={index}
-              data={listing}
-            />
-          ))}
-        </div>)
-      }
-    </Container>
+    <>
+      <Container>
+        {location ?
+          <SearchResults /> :
+          (<div
+            className="pt-52 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8"
+          >
+            {data.map((listing: any, index: number) => (
+              <ListingCard
+                key={index}
+                listIndex={index}
+                data={listing}
+              />
+            ))}
+          </div>)
+        }
+      </Container>
+      <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 mb-4 bg-black rounded-full">
+        <button
+          onClick={handleShowMapClick}
+          className=" text-white hover:shadow font-bold py-[14px] px-[19px] items-center">
+          <span className="items-center">
+            <span>
+              Show map
+            </span>
+            {/* <span>
+              <HiOutlineMap size={16} color='#fff' />
+            </span> */}
+          </span>
+        </button>
+      </div>
+    </>
   );
 };
 
